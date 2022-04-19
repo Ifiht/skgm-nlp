9379a8250cfe76d12ad68fc9e4730f166a4972ea
Michael <Ifiht@users.noreply.github.com>
2022-04-19 13:35:33 -0400

    update numbers, est. word count

--- core.rb
+++ core.rb
@@ -1,7 +1,7 @@
 # Require Ruby 3.1+
 # Ruby Actors (Ractor): https://ruby-doc.org/core-3.1.0/Ractor.html?msclkid=29975868c00111ecb8671c348bae9e5c
 # Ask Questions, until answer is not known
-
+# Ruby Async (vice threads): https://brunosutic.com/blog/async-ruby?msclkid=14618aa1bffe11ecb527a183ad865646
 require "async"
 
 # func() Is Word?
@@ -17,12 +17,15 @@ require "async"
 -- Source
 -- Confidence Factor
     - notionally 99.7% for system, 95.0% for creator, 67% for all others (until adjusted)
-
 :Object
 -- ID (IPv6 address?)
+    - 340,282,366,920,938,463,463,374,607,431,768,211,456 addresses, compare to ~750,000 words in English
+    - 6,909 languages spoken today,        ~5,181,750,000 words used by humans
+    - requires dynamic addressing schema
 -- Timestamp
 -- Names
--- Relations
+-- Group_Membership
+-- Object_Relations
 -- Actions?
 
 =end
