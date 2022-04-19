2cd04460ea0329ee766c4206ece5b8f5785f239d
Michael <Ifiht@users.noreply.github.com>
2022-04-19 14:01:08 -0400

    redis db schema?

--- core.rb
+++ core.rb
@@ -2,7 +2,17 @@
 # Ruby Actors (Ractor): https://ruby-doc.org/core-3.1.0/Ractor.html?msclkid=29975868c00111ecb8671c348bae9e5c
 # Ask Questions, until answer is not known
 # Ruby Async (vice threads): https://brunosutic.com/blog/async-ruby?msclkid=14618aa1bffe11ecb527a183ad865646
-require "async"
+require 'async'
+require 'redis'
+
+redis = Redis.new(host: "localhost")
+redis.set("a", 1)
+# "OK"
+redis.get("a")
+# Maintain DB as knowledge "web"?
+# What is the root? Give the web a single "truth" of confidence 100%?
+# Path to nodes decided by membership, reorganize optimally (BGP protocol?)
+
 
 # func() Is Word?
 
@@ -16,7 +26,8 @@ require "async"
 -- Value
 -- Source
 -- Confidence Factor
-    - notionally 99.7% for system, 95.0% for creator, 67% for all others (until adjusted)
+    - notionally 99.7% for system, 95.0% for creator, 67.0% for all others (until adjusted)
+
 :Object
 -- ID (IPv6 address?)
     - 340,282,366,920,938,463,463,374,607,431,768,211,456 addresses, compare to ~750,000 words in English
@@ -24,7 +35,10 @@ require "async"
     - requires dynamic addressing schema
 -- Timestamp
 -- Names
--- Group_Membership
+-- Object_Members
+    - not an array, inclusion is formulaic
+-- Object_Membership
+    - not an array, inclusion is formulaic
 -- Object_Relations
 -- Actions?
 
