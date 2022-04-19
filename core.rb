32017578a0c1bf6c5f7c7ed9f688a3d41cfe506e
Michael <Ifiht@users.noreply.github.com>
2022-04-19 13:03:40 -0400

    core template

---
+++ core.rb
@@ -0,0 +1,27 @@
+# Require Ruby 3.1+
+# Ruby Actors (Ractor): https://ruby-doc.org/core-3.1.0/Ractor.html?msclkid=29975868c00111ecb8671c348bae9e5c
+# Ask Questions, until answer is not known
+
+require "async"
+
+# func() Is Word?
+
+# func() Estimate Accuracy
+# .. Significant Figures go here
+
+#[[ Data Structure ]]
+=begin
+
+:Entry
+-- Value
+-- Source
+-- Confidence Factor
+
+:Object
+-- ID
+-- Timestamp
+-- Names
+-- Relations
+-- Actions?
+
+=end
